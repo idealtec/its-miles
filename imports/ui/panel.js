6e43b7fd8094bf9f739b08861717cb7f975a23c5
Yesh <YVelkuri@autoeveramerica.com>
2017-06-22 09:36:46 -0700

    Added user check

--- imports/ui/panel.js
+++ imports/ui/panel.js
@@ -1,16 +1,23 @@
 import Miles from '../../api/miles';
 import {Template} from 'meteor/templating';
 import {Meteor} from 'meteor/meteor';
+import {Session} from 'meteor/session';
 
 import './panel.html';
 
 
 Template.tmpl_main.helpers({
     isLoggedIn(){
+        Session.set('user',Meteor.userId());
         return Meteor.user();
     },
     allMiles(){
-        return Miles.find({});
+        return Miles.find({
+            author:Meteor.userId()
+        });
+    },
+    loggedUser(){
+        return Session.get('user');
     }
 });
 
